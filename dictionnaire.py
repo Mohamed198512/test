e569c59c016d623d1e26dacb40ef3ac5f1a4fb7b
Mohamed198512 <131703059+Mohamed198512@users.noreply.github.com>
2023-04-24 20:52:58 +0300

    Add files via upload

---
+++ dictionnaire.py
@@ -0,0 +1,9 @@
+chaussure={}
+chaussure["taille"]=42
+chaussure["marque"]="Nike"
+chaussure["race"]="berger_allemand"
+print(chaussure)
+chaussure.pop("race")
+print(chaussure)
+taille=chaussure["taille"]
+print(f"La taille de la chaussure est {taille}")
